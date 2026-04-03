aef305d8e51835fa1c390927caae1baba9e768c7
Pedro Luís <pedro.mellare@gmail.com>
2026-04-03 17:03:05 -0300

    V1.2 Tratamento de erros e novas funções

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 @app.route("/beecrowd/<int:id>")
 def solution(id):
     return render_template(f"{id}.html")
+
+@app.errorhandler(404)
+def notFound(error404):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def serverError(error500):
+    return render_template("500.html"), 500
